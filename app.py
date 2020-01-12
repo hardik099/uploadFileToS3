4ebf4152928c5a53db7b048791525eec21d57ecd
Hardik Gupta <hardik.gupta@flipkart.com>
2020-01-13 01:50:15 +0530

    adding files

--- app.py
+++ app.py
@@ -4,31 +4,87 @@ from flask import Flask, render_template, request, redirect, send_file, url_for
 
 from botoS3handler import fileToBbucket, fileFromBucket, allFileInBucket
 
+from botoDynamoDbHandler import saveInDynamoDB
+
+import openpyxl
+
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
 BUCKET = "insert_bucket_name_here"
 
+def validate_file(file):
+    return True;
+    if file.lower().endswith(('.csv', '.xlsx')):
+        if os.path.getsize(file) < 20000000:
+            return True;
+        else:
+            return False;
+    else:
+        return False;
+
+def validate_row(row_obj):
+    return True;
 
 @app.route('/')
 def entry_point():
-    return 'Hello World!'
+    return 'Today we will see an example of uploading a file to s3 then validating it and then storing it in dynamo db'
 
 
 @app.route("/file")
-def storage():
+def main():
     contents = allFileInBucket("flaskdrive")
-    return render_template('storage.html', contents=contents)
+    return render_template('main.html', contents=contents)
 
-
-@app.route("/uploadsuccess", methods=['POST'])
+@app.route("/upload", methods=['POST'])
 def upload():
     if request.method == "POST":
         f = request.files['file']
-        f.save(f.filename)
-        fileToBbucket(f"{f.filename}", BUCKET)
+        #f.save(f.filename)
 
-        return redirect("/file")
+        # validate file
+        if validate_file(f):
+            fileToBbucket(f"{f.filename}", BUCKET);
+        else:
+            # return because validation failed
+            return redirect("/validateFailed");
+
+        # show contents
+
+        return redirect(url_for('.fileContent', file=f));
+
+
+@app.route("/validateFailed")
+def validateFailed():
+    return render_template('validateFailed.html')
+
+@app.route("/fileContent")
+def fileContent():
+    if request.method == "POST":
+        # f = request.files['file']
+        # f.save(f.filename)
+        # text = open(f, 'r+');
+        # employeedata = text.read();
+        # text.close();
+        saveInDynamoDB(employeedata);
+        return redirect("/saveToDb");
+
+    file_data = [];
+    wb_obj = openpyxl.load_workbook(request.files['file']);
+    sheet_obj = wb_obj.active;
+    row_obj= sheet_obj.row(row = 1);
+    file_data.append(row_obj);
+    for row in sheet_obj.iter_rows(row_offset=1):
+        validate_row(row);
+        file_data.append(row_obj)
+    return render_template('fileContent.html', contents=file_data)
+
+
+@app.route("/saveToDb")
+def saveToDb(contents):
+    return render_template('saveToDb.html', contents=contents)
+
+
 
 
 @app.route("/download/<filename>", methods=['GET'])
---
+++ botoDynamoDbHandler.py
@@ -0,0 +1,32 @@
+import boto3
+
+dynamo_client = boto3.client('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+table_name = "employee_table"
+def saveInDynamoDB(employee_data):
+    return dynamo_client.batch_write_item(employee_data);
+    # dynamo=boto3.resource('dynamodb');
+    # t=dynamo.Table(table_name);
+    # for employee in employee_data:
+    #     t.put_item(employee)
+    #
+    # return "success";
+
+
+def readAllFromDynamoDB():
+    return dynamo_client.scan(
+        TableName=table_name
+    )
+
+def readBasedOnIdFromDynamoDB(employee_id):
+    try:
+        dynamo=boto3.resource('dynamodb');
+        t=dynamo.Table(table_name);
+        m=t.get_item(  #fetching the items from the table with a certain id
+            Key={
+                'EMPLOYEE_ID' : employee_id
+            })
+        m=m['Item'];
+        js = json.dumps(m)
+        return js;
+    except:
+        raise Exception("there is something wrong!!!")
